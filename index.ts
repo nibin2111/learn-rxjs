c4a5635d19931410da1aaabe23f97fdcc9a21ab3
Nibin <nibin2111@gmail.com>
2018-11-12 08:32:36 +0530

    Pilot version

---
+++ index.ts
@@ -0,0 +1,63 @@
+import { allReaders, allBooks } from './data';
+import { Observable, of, from, concat, fromEvent } from 'rxjs';
+
+/**
+ *  Obseravale.create or new Obsrvable 
+ * */
+let bookSubscriber$ = Observable.create((observer) => {
+    if(document.title != 'RXJS') {
+        observer.error('Not Rxjs');
+    }
+    for(let book of allBooks) {
+        observer.next(book);
+    }
+    setTimeout(() => {
+        observer.complete();
+    }, 2000);
+
+    return () => console.log('Inside observable');
+});
+
+// bookSubscriber$.subscribe((book:any)=> console.log(book.title));
+
+/*--------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+/**
+ *  of method for observables 
+ * */
+
+let source1$ = of('hello', 10, true, allReaders[0].name );
+// source1$.subscribe(value => console.log(value));
+
+/*--------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+/**
+ *  from method for observables 
+ * */
+
+// let source2$ = from(['hello', 10, true, allReaders[0].name]);
+// let source2$ = from(allReaders);
+let source2$ = concat(source1$, bookSubscriber$);
+from(source2$).subscribe(value => console.log(value));
+
+/*--------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+/**
+ *  fromEvent method for observables 
+ * */
+
+let button =  document.getElementById('readerBtn');
+fromEvent(button, 'click').subscribe(event => {
+    let readersListWrapper = document.getElementById('readersList');
+    for (let reader of allReaders) {
+        readersListWrapper.innerHTML += `${reader.name}<br>`;
+    }
+});
+/*--------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+/*
+Observers vs Subscribers
+    Observers - Receives the value
+    Subscribers - Produce value from  Observables.
+*/
+
